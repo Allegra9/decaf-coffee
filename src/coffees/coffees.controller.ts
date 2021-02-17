431c1618d800b7a090aee63f1f2da7ba5918b889
Allegra9 <aukseallegra@gmail.com>
2021-02-17 09:31:55 +0000

    Add Coffees service and entity

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,31 +7,35 @@ import {
   Delete,
   Patch,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
-  findAll(): string[] {
-    return ['mocha', 'espresso'];
+  findAll(): Coffee[] {
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() params): string {
-    return `Return #${params.id} coffee`;
+  findOne(@Param('id') id: string): Coffee {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
