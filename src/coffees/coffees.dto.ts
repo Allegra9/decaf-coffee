919ad453d752dafbd70271d2b239ceb133388371
Allegra9 <aukseallegra@gmail.com>
2021-02-17 20:32:08 +0000

    Add class-validator & mapped-types

--- src/coffees/coffees.dto.ts
+++ src/coffees/coffees.dto.ts
@@ -1,11 +1,15 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { IsString } from 'class-validator';
 export class CreateCoffeeDto {
+  @IsString()
   name: string;
+
+  @IsString()
   brand: string;
+
+  @IsString({ each: true }) // each value is a string
   flavors: string[];
 }
 
-export class UpdateCoffeeDto {
-  name?: string;
-  brand?: string;
-  flavors?: string[];
-}
+// same type as CreateCoffeeDto but with all of the properties set to optional
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
