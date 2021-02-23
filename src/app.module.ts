1b641694600456513477a8220a90e05ba37db0b8
Allegra9 <aukseallegra@gmail.com>
2021-02-23 19:56:52 +0000

    Add database container + TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'core',
+      username: 'postgres_user',
+      password: 'postgres_pass',
+      autoLoadEntities: true,
+      synchronize: true, // for dev only, disable for prod
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
